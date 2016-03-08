107104a2209911e1c3726a3e784aa33bc83e2f14
Cem Gencer <cem.gencer@me.com>
2016-03-08 14:39:03 +0200

    added test and build folders, creating build scripts

--- build/index.js
+++ build/index.js
@@ -1,7 +1,26 @@
-var fs = require('fs');
-var onlyScripts = require('./util/scriptFilter');
-var tasks = fs.readdirSync('./gulp/tasks/').filter(onlyScripts);
+(function() {
 
-tasks.forEach(function(task) {
-  require('./tasks/' + task);
-});
+	var fs = require('fs');
+	var onlyScripts = require('./util/scriptFilter');
+	var tasks = fs.readdirSync('./gulp/tasks/').filter(onlyScripts);
+
+	var files = {
+		npm:			'../package.json',
+		bower: 			'../bower.json',
+		test: 			'../test/index.js'
+	};
+	var paths = {
+		vendor: 		'../dist/js/vendor/',
+		data: 			'../app/data/',
+		fonts: 			'../app/fonts/',
+		scripts: 		'../app/scripts/',
+		styles: 		'../app/styles/',
+		data: 			'../app/data',
+
+	};
+
+	tasks.forEach(function(task) {
+		require('./tasks/' + task);
+	});
+
+}());
